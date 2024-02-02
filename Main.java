1dc6e7046200cb5b4b402f843b4c776c2782c84f
Gobind Singh <118898159+gobind9841@users.noreply.github.com>
2024-02-02 16:55:56 +0530

    Update Main.java
    Changes by gobind

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
 
-    int n = 20, firstTerm = 0, secondTerm = 1;
+    int n = 50, firstTerm = 0, secondTerm = 1;
     System.out.println("Fibonacci Series till " + n + " terms:");
 
     for (int i = 1; i <= n; ++i) {
