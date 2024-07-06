f30b3bf260433aa6e3bbe7f77d72defe04bb0a74
JAS PREET <89130631+thebugbounter@users.noreply.github.com>
2024-07-06 12:59:34 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 class Main {
   public static void main(String[] args) {
 
-    int n = 1, firstTerm = 0, secondTerm = 1;
+    int n = 10, firstTerm = 0, secondTerm = 1;
     System.out.println("Fibonacci Series till " + n + " terms:");
 
-    for (int i = 1; i <= 100; ++i) {
+    for (int i = 1; i <= n; ++i) {
       System.out.print(firstTerm + ", ");
 
       // compute the next term
